5b95672ef244bd72934256d5df265e3525a1093d
jacombo <zdenek.jakubec@gmail.com>
2018-01-06 13:43:12 +0100

    Add files via upload

---
+++ knihy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IFS=$'\n'
+for file in $(find . -type f -name "*.$1")
+do
+	COMMAND="ebook-convert \"${file}\" \"$(echo ${file} | sed "s/$1/$2/g")\" & "
+    echo ${COMMAND}
+    eval ${COMMAND}
+done
